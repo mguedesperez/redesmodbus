f2173d671420ba361d372e666d4695ca31cf9a90
Michelle Perez <guedesperez@gmail.com>
2020-11-24 19:08:18 -0300

    Server imprimindo Tempo

--- nome.py
+++ nome.py
@@ -1,14 +1,23 @@
 from pyModbusTCP.server import ModbusServer
+from pyModbusTCP.client import ModbusClient
+from datetime import datetime
 
 server = ModbusServer("127.0.0.1", 12345, no_block=True)
+aclient = ModbusClient(host="127.0.0.1", port=12345)
 
 try:
     print("Iniciando server..")
     server.start()
     print("Server online")
+    aclient.open()
+    
     while True:
+    
+        aclient.write_single_register(0,int(datetime.now().time().strftime("%S")))
+    
         continue
 except:
     print("Desligando server....")
+    aclient.close()
     server.stop()
     print("Server desligado")
